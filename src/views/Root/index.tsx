2183b9f7cec0806c1c49797efbabeea36a8455b2
Jason Aravanis <j.aravanis@icloud.com>
2022-04-24 18:28:11 +1000

    Replace HomeView with HomeContainer in RootView

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -1,14 +1,14 @@
 import { FC } from "react";
 import { Routes, Route } from "react-router-dom";
+import { HomeContainer } from "../../containers/Home";
 import { GameView } from "../Game";
-import { HomeView } from "../Home";
 import { OutcomeView } from "../Outcome";
 import { SetupView } from "../Setup";
 
 export const RootView: FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomeView />} />
+      <Route path="/" element={<HomeContainer />} />
       <Route path="/setup" element={<SetupView />} />
       <Route path="/game" element={<GameView />} />
       <Route path="/outcome" element={<OutcomeView />} />
