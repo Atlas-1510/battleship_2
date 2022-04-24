a670e269b2cd027ba25a2f81b3312f2c389bb79e
Jason Aravanis <j.aravanis@icloud.com>
2022-04-24 18:49:51 +1000

    Add tests for RootContainer

---
+++ src/containers/Root/__tests__/index.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { MemoryRouter, Router, Location } from "react-router-dom";
+import { RootContainer } from "..";
+import { GameContext } from "../../../contexts/Game";
+import { Game } from "../../../interfaces/Game";
+
+const setup = () => {
+  return render(
+    <MemoryRouter>
+      <RootContainer />
+    </MemoryRouter>
+  );
+};
+
+describe("RootContainer", () => {
+  test("On initial load, show home view", () => {
+    setup();
+    expect(
+      screen.getByRole("button", { name: "New Game" })
+    ).toBeInTheDocument();
+  });
+  test("Shows setup view after click on 'New Game' button", () => {
+    setup();
+    const startGameButton = screen.getByRole("button", {
+      name: "New Game",
+    });
+    user.click(startGameButton);
+    expect(screen.getByText("Place your ships")).toBeInTheDocument();
+  });
+});
