b995122456d328142e91efd690249f855d0e748c
Jason Aravanis <j.aravanis@icloud.com>
2022-04-24 18:58:42 +1000

    Swap SetupView for SetupContainer in RootView

--- src/views/Root/index.tsx
+++ src/views/Root/index.tsx
@@ -4,13 +4,13 @@ import { HomeContainer } from "../../containers/Home";
 import { routes } from "../../Routes";
 import { GameView } from "../Game";
 import { OutcomeView } from "../Outcome";
-import { SetupView } from "../Setup";
+import { SetupContainer } from "../../containers/Setup";
 
 export const RootView: FC = () => {
   return (
     <Routes>
       <Route path="*" element={<HomeContainer />} />
-      <Route path={routes.SETUP} element={<SetupView />} />
+      <Route path={routes.SETUP} element={<SetupContainer />} />
       <Route path={routes.GAME} element={<GameView />} />
       <Route path={routes.OUTCOME} element={<OutcomeView />} />
     </Routes>
