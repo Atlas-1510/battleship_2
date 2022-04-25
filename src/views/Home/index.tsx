86425b3061a100413263d8d7394df63ff6201d93
Jason Aravanis <j.aravanis@icloud.com>
2022-04-25 13:19:37 +1000

    Fix import bug in HomeView

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../../components/Button";
+import { Button } from "../../PresentationComponents/Button";
 
 interface Props {
   handleStartGame: React.MouseEventHandler;
